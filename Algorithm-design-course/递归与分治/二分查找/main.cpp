fe08e936392105aed70855b5d0a6ad03fadea5cd
jack-yong <1807413446@qq.com>
2021-11-29 21:44:05 +0800

    二分查找递归实现

--- Algorithm-design-course/递归与分治/二分查找/main.cpp
+++ Algorithm-design-course/递归与分治/二分查找/main.cpp
@@ -11,8 +11,26 @@ int BinarySearch(int* a,const int& x,int start,int end){
 	}
 	return -1;
 
+}
+
+//二分搜索法递归调用，下面还是以整数数组为例
+int BinarySearchRecursion(int* a,const int& x,int start,int end)
+{
+	if(start <= end){
+		int mid = (start+end)/2;
+		if(a[mid] == x) return mid;
+		else if(a[mid]>x) BinarySearchRecursion(a,x,start,mid-1);
+		else BinarySearchRecursion(a,x,mid+1,end);
+	}
+	else{
+		return -1;
+	}
 } 
+
 int main(int argc, char *argv[]) {
 	int a[10] = {1,2,3,4,5,6,7,8,9,11};
-	cout<<BinarySearch(a,11,0,9);
+	//非递归调用 
+	//cout<<BinarySearch(a,11,0,9);
+	//递归调用
+	 cout<<BinarySearchRecursion(a,1,0,9);
 }
---
+++ Algorithm-design-course/递归与分治/归并排序/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+
+//下面是归并排序的相关算法 
+
+int main(int argc, char *argv[]) {
+	return 0;
+}
