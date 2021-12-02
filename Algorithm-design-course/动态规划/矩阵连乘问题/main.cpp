600561f1e9bd672ba92f27b51daccb5c758d9be1
jack-yong <1807413446@qq.com>
2021-12-02 21:54:24 +0800

    矩阵连乘动态规划to be continue

---
+++ Algorithm-design-course/动态规划/矩阵连乘问题/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+ 
+//计算最优值的动态规划算法todo
+#define NUM 51
+int p[NUM];
+int m[NUM][NUM];
+int n[NUM][NUM];
+
+void MatrixChain(int n)
+{
+
+	
+}
+
+int main(int argc, char *argv[]) {
+	return 0;
+}
