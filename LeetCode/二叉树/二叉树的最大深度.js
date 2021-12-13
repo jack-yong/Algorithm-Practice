4af4869c924df814603a843382d39288fb3980a8
jack-yong <1807413446@qq.com>
2021-12-13 10:37:21 +0800

    二叉树的最大深度修改解释

--- LeetCode/二叉树/二叉树的最大深度.js
+++ LeetCode/二叉树/二叉树的最大深度.js
@@ -2,7 +2,7 @@
  * @Author: cyong
  * @Date: 2021-12-13 10:32:16
  * @LastEditors: cyong
- * @LastEditTime: 2021-12-13 10:33:56
+ * @LastEditTime: 2021-12-13 10:36:25
  * @FilePath: \LeetCode\二叉树\二叉树的最大深度.js
  * @Description: 给定一个二叉树，找出其最大深度。二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
  */
@@ -10,7 +10,7 @@
 
 /**
  * @author: cyong
- * @description: 使用的是深度优先遍历，但是时间复杂度很大，js中尽量定义变量来接受递归的参数，否则执行效率很低
+ * @description: 使用的是深度优先遍历，但是时间复杂度很大，js中尽量定义变量来接受递归的返回值，否则执行效率很低
  * @param {*} root
  * @return {number}
  */
